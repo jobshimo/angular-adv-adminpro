0f30096d1c74c987e33d7411c09683ad99bcb3fc
Martín Beranl <job_tincho@hotmail.com>
2020-10-08 20:16:52 +0200

    Fin seccion 7

---
+++ src/app/services/settings.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SettingsService {
+  private linkTheme = document.querySelector('#theme');
+
+  constructor() {
+    const localTheme =
+      localStorage.getItem('theme') || './assets/css/colors/default-dark.css';
+    this.linkTheme.setAttribute('href', localTheme);
+  }
+
+  changeTheme(theme: string) {
+    const urlTheme = `./assets/css/colors/${theme}.css`;
+    this.linkTheme.setAttribute('href', urlTheme);
+
+    localStorage.setItem('theme', urlTheme);
+    this.checkCurrentTheme();
+  }
+
+  checkCurrentTheme() {
+    const links = document.querySelectorAll('.selector');
+    links.forEach((elem) => {
+      elem.classList.remove('working');
+      const btnTheme = elem.getAttribute('data-theme');
+      const btnThemeUrl = `./assets/css/colors/${btnTheme}.css`;
+      const currentTheme = this.linkTheme.getAttribute('href');
+
+      if (btnThemeUrl === currentTheme) {
+        elem.classList.add('working');
+      }
+    });
+  }
+}
